1871deedb72ff1f7a81137a4be4cf0f5b39f275c
Jaren Knodel <Jaren@Knodel.com>
2025-02-15 08:06:43 -0500

    Multi device support

--- gvoice_page.js
+++ gvoice_page.js
@@ -38,6 +38,9 @@ const fromRegexp = /^From: (.*) <sip:\+?(\d+|anonymous)\@/m;
                 let [, _, number] = text.match(fromRegexp);
                 this._pws.send(JSON.stringify(["stopRinging", number]));
             }
+            if (text.startsWith("BYE ")) {
+                this._pws.send(JSON.stringify(["end"]));
+            }
         }
 
         _connect = () => {
@@ -52,17 +55,18 @@ const fromRegexp = /^From: (.*) <sip:\+?(\d+|anonymous)\@/m;
                 let [typ, data] = JSON.parse(e.data);
                 
                 if (typ === "answer") {
+                    await changeAudioDevices(data.device.serial);
                     document.querySelector(".pickup-call-button-container button").click();
-                    this._pws.send(JSON.stringify(["stopRinging", data]));
                 }
     
                 if (typ === "end") {
-                    document.querySelector("button.call-end-button").click()
+                    document.querySelector("button.call-end-button")?.click()
                 }
     
                 if (typ === "call") {
+                    await changeAudioDevices(data.device.serial);
                     let input = document.querySelector("gv-make-call-panel input");
-                    input.value = data;
+                    input.value = data.number;
                     input.dispatchEvent(new Event("input"));
                     document.querySelector("gv-make-call-panel .input-row button").click();
                 }
@@ -71,4 +75,50 @@ const fromRegexp = /^From: (.*) <sip:\+?(\d+|anonymous)\@/m;
     }
 
     window.WebSocket = WebSocket;
+
+    let storageOverrides = new Map();
+
+    async function changeAudioDevice(type, serial) {
+        let devices = (await navigator.mediaDevices.enumerateDevices()).filter(d => d.kind === `audio${type}`);
+
+        let device = devices.find(d => d.label.includes(serial));
+        if (!device) {
+            let genericDevices = devices.filter(s => s.label.includes("USB Internet Phone"));
+            if (genericDevices.length === 0) {
+                console.error(`Unable to find an ${type} device to switch to.`, devices);
+                return;
+            }
+            if (genericDevices.length > 1) {
+                console.warn(`The ${type} device for ${serial} could not be found directly, and multiple phone devices exist.`, genericDevices);
+            }
+
+            option = genericDevices[0];
+        }
+
+        let key = `gv::${window._gv.soyProto.UserInfo[1]}::audio-${type}-device-id`;
+        let newValue = device.deviceId;
+
+        console.log(key, newValue)
+        storageOverrides.set(key, newValue);
+
+        window.dispatchEvent(new StorageEvent("storage", {
+            key,
+            newValue,
+            url: location.toString()
+        }));
+    }
+
+    async function changeAudioDevices(serial) {
+        changeAudioDevice("input", serial);
+        changeAudioDevice("output", serial);
+    }
+
+    let localStorageGetItem = localStorage.getItem.bind(localStorage);
+    localStorage.getItem = function (keyName) {
+        if (storageOverrides.has(keyName)) {
+            return storageOverrides.get(keyName);
+        }
+
+        return localStorageGetItem(keyName);
+    }
 })();
